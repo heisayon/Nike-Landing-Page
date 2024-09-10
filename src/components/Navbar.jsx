826e979311c626cb4bd11df5b254945fad826c43
heisayon <heisayon@gmail.com>
2024-09-10 11:52:32 +0100

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import { headerLogo } from "../../public/assets/images";
 import { navlinks } from "../constants";
-import { hamburger } from "../../public/assets/icons";
+import { close, hamburger } from "../../public/assets/icons";
+import { useState } from "react";
 function Navbar() {
+  const [mobile, setMobile] = useState(false);
   return (
-    <div className="pt-5">
+    <div className="pt-5 ">
       <ul className="flex items-center justify-between">
         <li>
           <img src={headerLogo} alt="Nike Logo" />
@@ -18,8 +20,30 @@ function Navbar() {
           })}
         </div>
         <li className="font-semibold max-xl:hidden text-xl">Explore</li>
-        <img src={hamburger} alt="Hamburger" className="xl:hidden w-6 h-6" />
+        <img
+          src={hamburger}
+          alt="Hamburger"
+          className="xl:hidden w-6 h-6" 
+          onClick={() => setMobile(!mobile)}
+        />
       </ul>
+
+      {mobile && (
+        <div className="fixed top-0 right-0 w-[50%] h-[100%] xl:hidden bottom-0 z-[100] bg-white px-5 py-3 border-l border-[#4d4c4c]">
+          <div className="flex justify-end mb-4">
+            <img
+              src={close}
+              onClick={() => setMobile(false)}
+              className="text-3xl font-semibold font-Palanquin w-6 h-6 mt-5" 
+            />
+          </div>
+          <ul className="flex flex-col items-center gap-7 justify-center mt-52">
+            {navlinks.map((links, index) => {
+              return <li key={index} className="hover:opacity-70 font-medium">{links}</li>;
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
